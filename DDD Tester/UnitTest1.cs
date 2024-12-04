f87348d0d319122a7cdf1e53a8b6be747c513edb
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2024-12-04 12:03:09 +0000

    forgot to add these files

---
+++ DDD Tester/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+namespace DDD_Tester;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestUserName()
+    {
+     string expectedName = "JohnDoe";
+     string actualName = Environment.UserName;
+     Assert.AreEqual(expectedName, actualName);    
+    }
+}
